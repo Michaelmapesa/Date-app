e70e3d2817ad8cb4cc76fae63a97b205f5881683
michael mapesa <michaelmapesa93@gmail.com>
2021-12-11 18:28:15 +0300

    add more function

--- index.js
+++ index.js
@@ -3,15 +3,35 @@
 //function myFunction() {
   //  document.getElementById("form1").submit();
   //}
- 
-  const submit = document.getElementById('submit');
-  let day=document.getElementById("uname1");
-  let gender=document.getElementById("uname");
-  var Yaw;
 
-submit.addEventListener('click', function(){
- if (day.value==07/01/1993)
-     alert("Yaw")
-}
+  var day=d.getDay(document.getElementById("uname1"));
+  var gender=document.getElementById("uname");
+  var a=male;
+  var b=female;
+
+  
+  function myFunction() {
+       if(day==0 && gender==a){
+         alert("Your name is Kwasi");
+
+       }else if(day==1 && gender==a){
+         alert("Your name is Kwadwo");
+       
+       }else if(day==2 && gender==a){
+        alert("Your name is Kwabena");
+
+      }else if(day==3 && gender==a){
+        alert("Your name is Kwaku");
+
+      }else if(day==4 && gender==a){
+        alert("Your name is Yaw");
+
+      }else if(day==5 && gender==a){
+        alert("Your name is Koti");
+
+      }else 
+        alert("Your name is Kwame");
+      
+  }
   
     
