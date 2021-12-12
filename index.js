414a6b2cbb1f656fb14fc0a53f2734decb09af7e
michael mapesa <michaelmapesa93@gmail.com>
2021-12-12 14:20:22 +0300

    modify function

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
 
+ 
+ var d= new Date();
+ var day=days[d.getDay(document.getElementById("uname1"))];
+ var day =d.getDay(document.getElementById("uname1"));
+ var gender=document.getElementById("uname");
+ var male=a;
+ var female=b;
   
-  var d= new Date();
-  var day=d.getDay(document.getElementById("uname1"));
-  var gender=document.getElementById("uname");
-  var a=male;
-  var b=female;
-
-  
-  function myFunction() {
-       if(day==0 && gender==a){
-         alert("Your name is Kwasi");
-
-       }else if(day==1 && gender==a){
-         alert("Your name is Kwadwo");
-       
-       }else if(day==2 && gender==a){
-        alert("Your name is Kwabena");
-
-      }else if(day==3 && gender==a){
-        alert("Your name is Kwaku");
-
-      }else if(day==4 && gender==a){
-        alert("Your name is Yaw");
-
-      }else if(day==5 && gender==a){
-        alert("Your name is Koti");
-
-      }else if(day==6 && gender==a){
-        alert("Your name is Kwame");
-
-      }else if(day==1 && gender==b){
-        alert("Your name is Akosua");
+  function myFunction(){
+    if(day==0 && gender==a){
+      alert("Your name is Kwasi");
       
-      }else if(day==2 && gender==b){
-       alert("Your name is Adwoa");
+    }else if(day==1 && gender==a){
+      alert("Your name is Kwadwo");
+    
+    }else if(day==2 && gender==a){
+     alert("Your name is Kwabena");
 
-     }else if(day==3 && gender==b){
-       alert("Your name is Abenaa");
+   }else if(day==3 && gender==a){
+     alert("Your name is Kwaku");
 
-     }else if(day==4 && gender==b){
-       alert("Your name is Akua");
+   }else if(day==4 && gender==a){
+     alert("Your name is Yaw");
 
-     }else if(day==5 && gender==b){
-       alert("Your name is Yaa");
+   }else if(day==5 && gender==a){
+     alert("Your name is Koti");
 
-     }else if(day==5 && gender==b){
-       alert("Your name is Afua");
+   }else if(day==6 && gender==a){
+     alert("Your name is Kwame");
 
-      }else
-        alert("Your name is Ama");
-          
+   }else if(day==1 && gender==b){
+     alert("Your name is Akosua");
+   
+   }else if(day==2 && gender==b){
+    alert("Your name is Adwoa");
+
+  }else if(day==3 && gender==b){
+    alert("Your name is Abenaa");
+
+  }else if(day==4 && gender==b){
+    alert("Your name is Akua");
+
+  }else if(day==5 && gender==b){
+    alert("Your name is Yaa");
+
+  }else if(day==5 && gender==b){
+    alert("Your name is Afua");
+
+   }else{
+    alert("Your name is Ama");   
+   }
+        
   }
+
+       
+     
   
     
