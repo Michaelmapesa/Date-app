1e355a667c4e20dfb76559e55f5a39ab9727b392
michael mapesa <michaelmapesa93@gmail.com>
2021-12-21 21:56:46 +0300

    modify function

--- index.js
+++ index.js
@@ -1,59 +1,88 @@
 
+ var Day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+ var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
  
- var d= new Date();
- var day=days[d.getDay(document.getElementById("uname1"))];
- var day =d.getDay(document.getElementById("uname1"));
- var gender=document.getElementById("uname");
- var male=a;
- var female=b;
+ var myForm = document.getElementById("form1");
+ myForm.addEventListener("submit",(e)=>{
+   e.preventDefault();
+   var birthDate = document.getElementById("uname1").value ;
+   birthDate=new Date(birthDate);
+   var gender=document.getElementById("sel1").value;
+   var birthYear=birthDate.getFullYear();
+   var birthMonth=birthDate.getMonth()+1;
+   var birthDay=birthDate.getDate();
+   var MM=birthMonth;
+   var CC=birthYear.toString().substr(0,2);
+   var YY=birthYear.toString().substr(2,2);
+   var MM=birthMonth;
+   var DD=birthDay;
+   CC = parseInt(CC);
+   YY = parseInt(YY);
+   MM = parseInt(MM);
+   DD = parseInt(DD);
   
-  function myFunction(){
-    if(day==0 && gender==a){
+   weeksDay=(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+   weeksDay=parseInt(weeksDay)-1;
+
+    if(weeksDay==0 && gender=="male"){
       alert("Your name is Kwasi");
       
-    }else if(day==1 && gender==a){
+    }else if(weeksDay==1 && gender=="male"){
       alert("Your name is Kwadwo");
     
-    }else if(day==2 && gender==a){
-     alert("Your name is Kwabena");
+    }else if(weeksDay==2 && gender=="male"){
+      alert("Your name is Kwabena");
 
-   }else if(day==3 && gender==a){
-     alert("Your name is Kwaku");
+    }else if(weeksDay==3 && gender=="male"){
+      alert("Your name is Kwaku");
 
-   }else if(day==4 && gender==a){
-     alert("Your name is Yaw");
+    }else if(weeksDay==4 && gender=="male"){
+      alert("Your name is Yaw");
 
-   }else if(day==5 && gender==a){
-     alert("Your name is Koti");
+    }else if(weeksDay==5 && gender=="male"){
+      alert("Your name is Kofi");
 
-   }else if(day==6 && gender==a){
-     alert("Your name is Kwame");
+    }else if(weeksDay==6 && gender=="male"){
+      alert("Your name is Kwame");
 
-   }else if(day==1 && gender==b){
-     alert("Your name is Akosua");
+    }else if(weeksDay==0 && gender=="female"){
+      alert("Your name is Akosua");
    
-   }else if(day==2 && gender==b){
-    alert("Your name is Adwoa");
+    }else if(weeksDay==1 && gender=="female"){
+      alert("Your name is Adwoa");
 
-  }else if(day==3 && gender==b){
-    alert("Your name is Abenaa");
+    }else if(weeksDay==2 && gender=="female"){
+      alert("Your name is Abenaa");
 
-  }else if(day==4 && gender==b){
-    alert("Your name is Akua");
+    }else if(weeksDay==3 && gender=="female"){
+      alert("Your name is Akua");
 
-  }else if(day==5 && gender==b){
-    alert("Your name is Yaa");
+    }else if(weeksDay==4 && gender=="female"){
+      alert("Your name is Yaa");
 
-  }else if(day==5 && gender==b){
-    alert("Your name is Afua");
+    }else if(weeksDay==5 && gender=="female"){
+      alert("Your name is Afua");
 
-   }else{
-    alert("Your name is Ama");   
-   }
-        
-  }
+    }else if(weeksDay==6 && gender=="female"){
+      alert("Your name is Ama");   
+    }else{
+      return false;
+    }
+    document.getElementById("form1").reset();
+    
+      
+  });
+  
 
        
      
   
     
+
+ 
+
+
+
+
+
