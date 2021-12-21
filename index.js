d1c57587d010bba3bb61c9cf063dc10472421b04
michael mapesa <michaelmapesa93@gmail.com>
2021-12-21 17:04:11 +0300

    add array,day,femaleName and maleName

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 
  
- var d= new Date();
- var day=days[d.getDay(document.getElementById("uname1"))];
- var day =d.getDay(document.getElementById("uname1"));
- var gender=document.getElementById("uname");
- var male=a;
- var female=b;
-  
-  function myFunction(){
-    if(day==0 && gender==a){
+ var Day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+ var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+ 
+ 
+
+
+  function myFunction(day,gender){
+    if(day==0 && gender=="male"){
       alert("Your name is Kwasi");
       
-    }else if(day==1 && gender==a){
+    }else if(day==1 && gender=="male"){
       alert("Your name is Kwadwo");
     
-    }else if(day==2 && gender==a){
+    }else if(day==2 && gender=="male"){
      alert("Your name is Kwabena");
 
-   }else if(day==3 && gender==a){
+   }else if(day==3 && gender=="male"){
      alert("Your name is Kwaku");
 
-   }else if(day==4 && gender==a){
+   }else if(day==4 && gender=="male"){
      alert("Your name is Yaw");
 
-   }else if(day==5 && gender==a){
-     alert("Your name is Koti");
+   }else if(day==5 && gender=="male"){
+     alert("Your name is Kofi");
 
-   }else if(day==6 && gender==a){
+   }else if(day==6 && gender=="male"){
      alert("Your name is Kwame");
 
-   }else if(day==1 && gender==b){
+   }else if(day==1 && gender=="female"){
      alert("Your name is Akosua");
    
-   }else if(day==2 && gender==b){
+   }else if(day==2 && gender=="female"){
     alert("Your name is Adwoa");
 
-  }else if(day==3 && gender==b){
+  }else if(day==3 && gender=="female"){
     alert("Your name is Abenaa");
 
-  }else if(day==4 && gender==b){
+  }else if(day==4 && gender=="female"){
     alert("Your name is Akua");
 
-  }else if(day==5 && gender==b){
+  }else if(day==5 && gender=="female"){
     alert("Your name is Yaa");
 
-  }else if(day==5 && gender==b){
+  }else if(day==6 && gender=="female"){
     alert("Your name is Afua");
 
-   }else{
+  }else{
     alert("Your name is Ama");   
-   }
-        
+      
   }
+  myFunction(day,gender);
 
        
      
